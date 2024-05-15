7d8e35a5301cb7cf83fbf386f2f2f8651fab96d0
abwi_dev <wbacha95@gmail.com>
2024-05-16 00:42:19 +0200

    [WIP] Added translation feature

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,17 @@
 console.log('Hello adazdazd script.js');
 
-var map = L.map('map').setView([48.688856920786336, 2.18066078674733], 16);
+var map = L.map('map').setView([49.43693086050762, 1.102400429307783], 15);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
     }).addTo(map);
 
-    var marker = L.marker([48.688856920786336, 2.18066078674733]).addTo(map);
+    var marker = L.marker([49.43693086050762, 1.102400429307783]).addTo(map);
     marker.on('click', function() {
-        marker.bindPopup('Hello, I am a popup!').openPopup();
+        marker.bindPopup('Hello, I am Marine').openPopup();
+    });
+
+    var marker = L.marker([49.43758665623901, 1.1034924859261053]).addTo(map);
+    marker.on('click', function() {
+        marker.bindPopup('Hospital').openPopup();
     });
