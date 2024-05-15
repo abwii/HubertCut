f5e7f5257bcc43160903967f5cfcbb35dabca7f5
Wassim <wassim.bacha@adventori.com>
2024-05-15 11:59:21 +0200

    added redac, better map

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,12 @@
-console.log('Hello from script.js');
+console.log('Hello adazdazd script.js');
 
 var map = L.map('map').setView([48.688856920786336, 2.18066078674733], 16);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
     }).addTo(map);
+
+    var marker = L.marker([48.688856920786336, 2.18066078674733]).addTo(map);
+    marker.on('click', function() {
+        marker.bindPopup('Hello, I am a popup!').openPopup();
+    });
