0de34c394a5a7146b03fc7c0c9785371f992bd75
abwi_dev <wbacha95@gmail.com>
2024-05-15 03:17:38 +0200

    added map test

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,7 @@
 console.log('Hello from script.js');
+
+var map = L.map('map').setView([48.688856920786336, 2.18066078674733], 16);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+    }).addTo(map);
