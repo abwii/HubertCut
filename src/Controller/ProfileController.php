afba52454a57f1f01c4aec80f08a03f1f09fa93e
abwi_dev <wbacha95@gmail.com>
2024-06-22 02:16:44 +0200

    Customisable profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
+use App\Form\ChangeNameFormType;
+use App\Form\ChangePhoneFormType;
+use App\Form\ChangeEmailFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $user = $this->getUser();
+
+        $nameForm = $this->createForm(ChangeNameFormType::class, $user);
+        $phoneForm = $this->createForm(ChangePhoneFormType::class, $user);
+        $emailForm = $this->createForm(ChangeEmailFormType::class, $user);
+
+        $nameForm->handleRequest($request);
+        $phoneForm->handleRequest($request);
+        $emailForm->handleRequest($request);
+
+        if ($nameForm->isSubmitted() && $nameForm->isValid()) {
+            $entityManager->flush();
+            return $this->redirectToRoute('app_profile');
+        }
+
+        if ($phoneForm->isSubmitted() && $phoneForm->isValid()) {
+            $entityManager->flush();
+            return $this->redirectToRoute('app_profile');
+        }
+
+        if ($emailForm->isSubmitted() && $emailForm->isValid()) {
+            $entityManager->flush();
+            return $this->redirectToRoute('app_profile');
+        }
+
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'nameForm' => $nameForm->createView(),
+            'phoneForm' => $phoneForm->createView(),
+            'emailForm' => $emailForm->createView(),
         ]);
     }
 }
---
+++ src/Form/ChangeNameFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ChangeNameFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([]);
+    }
+}
